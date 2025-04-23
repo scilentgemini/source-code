d0f7c8ef4044637281df446971618617ca6eaf7a
scilentgemini <rbchitthi@gmail.com>
2025-04-23 21:35:36 +0545

    header and footer updated

--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -8,17 +8,18 @@
                     <ul class="wsus__topbar_left">
                         <li><a href="mailto:{{ config('settings.site_email') }}"><i class="fal fa-envelope"></i>
                                 {{ config('settings.site_email') }}</a></li>
-                        <li><a href="callto:{{ config('settings.site_phone') }}"><i class="fal fa-phone-alt"></i>{{ config('settings.site_phone') }}</a></li>
+                        <li><a href="callto:{{ config('settings.site_phone') }}"><i
+                                    class="fal fa-phone-alt"></i>{{ config('settings.site_phone') }}</a></li>
                     </ul>
                 </div>
                 <div class="col-xl-6 col-md-5">
                     <div class="wsus__topbar_right">
-                        @auth
+                        {{-- @auth
                             <a href="{{ route('user.dashboard') }}"><i class="fas fa-user"></i> Dashboard</a>
                         @endauth
                         @guest
-                        <a href="{{ route('login') }}"><i class="fas fa-user"></i> Login</a>
-                        @endguest
+                            <a href="{{ route('login') }}"><i class="fas fa-user"></i> Login</a>
+                        @endguest --}}
                     </div>
                 </div>
             </div>
@@ -45,27 +46,33 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav m-auto">
                     @foreach (Menu::getByName('Main Menu') as $menu)
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ $menu['link'] }}" >
-                            {{ $menu['label'] }}
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ $menu['link'] }}">
+                                {{ $menu['label'] }}
 
+                                @if ($menu['child'])
+                                    <i class="far fa-chevron-down"></i>
+                                @endif
+                            </a>
                             @if ($menu['child'])
-                                <i class="far fa-chevron-down"></i>
+                                <ul class="menu_droapdown">
+                                    @foreach ($menu['child'] as $child)
+                                        <li><a href="{{ $child['link'] }}">{{ $child['label'] }}</a></li>
+                                    @endforeach
+                                </ul>
                             @endif
-                        </a>
-                        @if ($menu['child'])
-                        <ul class="menu_droapdown">
-                            @foreach ($menu['child'] as $child)
-                                <li><a href="{{ $child['link'] }}">{{ $child['label'] }}</a></li>
-                            @endforeach
-                        </ul>
-                        @endif
-                    </li>
+                        </li>
                     @endforeach
 
 
                 </ul>
-                <a class="user_btn" href="{{ route('user.listing.create') }}"><i class="far fa-plus"></i> add listing</a>
+                @auth
+                    <a class="user_btn" href="{{ route('user.dashboard') }}"><i class="fas fa-user"></i> Dashboard</a>
+                @endauth
+                @guest
+                    <a class="user_btn" href="{{ route('login') }}"><i class="fas fa-user"></i> Login</a>
+                @endguest
+                {{-- <a class="user_btn" href="{{ route('user.listing.create') }}"><i class="far fa-plus"></i> add listing</a> --}}
             </div>
         </div>
     </nav>
