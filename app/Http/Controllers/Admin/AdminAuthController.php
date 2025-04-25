dac3180c91356d446a0e9ebea125b08894cee9a5
scilentgemini <rbchitthi@gmail.com>
2025-04-25 10:35:57 +0545

    admin/login redirects to /login

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class AdminAuthController extends Controller
 {
-    function login() : View
+    function login() : RedirectResponse
     {
-        return view('admin.auth.login');
+        return redirect()->route('login');
     }
 
     function PasswordRequest() : View {
