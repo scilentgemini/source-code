0811e8d5ccf572d72d28ca531cd8f72b14bdb980
scilentgemini <rbchitthi@gmail.com>
2025-04-26 14:23:08 +0545

    updated button in admin/profile to route to dashboard

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -4,7 +4,7 @@
     <section class="section">
         <div class="section-header">
             <div class="section-header-back">
-                <a href="features-posts.html" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+                <a href="{{ route('admin.dashboard.index') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
             </div>
             <h1>Profile</h1>
             <div class="section-header-breadcrumb">
